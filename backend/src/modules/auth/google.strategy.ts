b19ca349b7566da7624c00a8be69b8e3f9b6f141
Eng-Moaz-Tello <shangahay1995@gmail.com>
2026-05-06 12:21:47 +0300

    update urls

--- backend/src/modules/auth/google.strategy.ts
+++ backend/src/modules/auth/google.strategy.ts
@@ -17,7 +17,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       scope: ['email', 'profile'],
     });
   }
-
+  
   async validate(
     accessToken: string,
     refreshToken: string,
